5c7eefd52bdf9c92900f4fc2c01b8aa7de3bf448
Rudi Rocha <rudi.rocha@gmail.com>
2022-11-12 00:41:51 +0000

    Add Find projects function

--- application/src/Handler/InitializeSolutionHandler.php
+++ application/src/Handler/InitializeSolutionHandler.php
@@ -4,12 +4,14 @@ namespace PhpOrchestra\Application\Handler;
 
 use InvalidArgumentException;
 use PhpOrchestra\Application\Handler\CommandHandlerInterface;
+use PhpOrchestra\Domain\Entity\Project;
 use PhpOrchestra\Domain\Entity\Solution;
 
 class InitializeSolutionHandler implements CommandHandlerInterface
 {
     private readonly Solution $solution;
-    private bool $scanforProjects = false;
+    private bool $scanForProjects = false;
+    private int $scanForProjectsDepth = 0;
 
     public function setSolution(Solution $solution): self
     {
@@ -20,7 +22,14 @@ class InitializeSolutionHandler implements CommandHandlerInterface
 
     public function doProjectScan(bool $flag): self
     {
-        $this->scanforProjects = $flag;
+        $this->scanForProjects = $flag;
+        return $this;
+    }
+
+    public function setProjectScanDirectoryDepth(int $depth = 0) : self
+    {
+        $this->scanForProjectsDepth = $depth;
+
         return $this;
     }
 
@@ -34,10 +43,55 @@ class InitializeSolutionHandler implements CommandHandlerInterface
             throw new InvalidArgumentException(sprintf('[%s] already exists.', $this->solution->getFullPath()));
         }
 
-        if($this->scanforProjects) {
+        if ($this->scanForProjects) {
             // to do: add logic
+        $this->setProjectScanDirectoryDepth(1);
+           $projects = $this->findProjects($this->solution->getPath());
+            var_dump($projects);
+            die('== END SCAN PROJECTS ==');
         }
 
         file_put_contents($this->solution->getFullPath(), json_encode($this->solution->toArray(), JSON_PRETTY_PRINT));
     }
+
+    /**
+     * Finding projects is about searching here a composer.json file is in.
+     * This function should find all the projects starting from the orchestra.json file directory
+     */
+    private function findProjects(string $baseDir, int $depthLevel = 0)
+    {
+        $excludedDirectories = ['.', '..', 'vendor', '.git', '.vscode', '.idea'];
+        $projectsFound = [];
+
+        if ($this->scanForProjectsDepth >= $depthLevel) {
+
+            $allFiles = scandir($baseDir);
+
+            foreach ($allFiles as $childDir) {
+                $currentDirectory = sprintf('%s%s%s', $baseDir, DIRECTORY_SEPARATOR, $childDir);
+
+                if (is_dir($currentDirectory) && !in_array($childDir, $excludedDirectories)) {
+                    echo 'checking '.$currentDirectory . PHP_EOL;
+                    if ($this->hasComposerFile($currentDirectory)) {
+                        // create Project entity and return
+                        $project = new Project();
+                        $project->setName($childDir)->setPath($currentDirectory);
+                        $projectsFound[] = $project;
+
+
+                    } else {
+                        $projectsFound = array_merge($projectsFound, $this->findProjects($currentDirectory, ++$depthLevel));
+                    }
+
+                }
+            }
+        }
+
+        return $projectsFound;
+    }
+
+    private function hasComposerFile($path): bool
+{
+    return is_file(sprintf('%s%scomposer.json', $path, DIRECTORY_SEPARATOR));
+}
 }
